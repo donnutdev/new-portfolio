8a161a6156a9f81c35dca55eddf570ecb414e94c
Rishab <skrishab00@gmail.com>
2024-10-27 09:51:49 +0530

    first

---
+++ src/lib/components/ui/card/card-header.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { cn } from "$lib/utils.js";
+	let className = undefined;
+	export { className as class };
+</script>
+
+<div class={cn("inline-flex space-x-2 p-6 pb-0 place-items-center", className)} {...$$restProps}>
+	<slot />
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,106 @@
+<script>
+    import '../app.css';
+    let { children, data } = $props();
+    import { ModeWatcher } from "mode-watcher";
+    import { afterNavigate, beforeNavigate } from "$app/navigation";
+    import {tweened} from "svelte/motion";
+
+    let { global } = data;
+
+    let scrollY = $state(0);
+    let navBar;
+
+    let navigating = $state(false);
+    let navigationProgress = tweened(0, { duration: 300 });
+
+    beforeNavigate((nav) => {
+        if (nav.willUnload === true) return;
+        navigating = true;
+        navigationProgress.set(65);
+    });
+
+    afterNavigate(() => {
+        navigationProgress.set(100);
+        navigationProgress.subscribe(value => {
+            if (value === 100) {
+                setTimeout(() => {
+                    navigating = false;
+                    navigationProgress.set(0);
+                }, 500);
+            }
+        });
+    });
+
+    $effect(() => {
+        if (scrollY > 0) {
+            navBar.classList.add('shadow', 'backdrop-blur-lg', 'bg-black', 'bg-opacity-20');
+        } else {
+            navBar.classList.remove('shadow', 'backdrop-blur-lg', 'bg-black', 'bg-opacity-20');
+        }
+    })
+</script>
+
+<svelte:window bind:scrollY/>
+
+<svelte:head>
+    <title>{global.title}</title>
+</svelte:head>
+
+<ModeWatcher defaultTheme="dark"/>
+<nav class="text-white w-full transition-all duration-500 inline-flex justify-center p-4 fixed z-50" bind:this={navBar}>
+    <div
+            class="h-0.5 bg-gradient-to-r from-cyan-400 to-indigo-600 absolute top-0 left-0"
+            style:width={$navigationProgress/100*100 + "%"}
+            hidden={!navigating}
+    ></div>
+    <ul class="inline-flex gap-8">
+        <li>
+            <a href="/" class="group relative transition duration-300">
+                <span class="group-hover:text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-indigo-600">
+                    About
+                </span>
+                <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-gradient-to-r from-cyan-400 to-indigo-600"></span>
+                <span class="absolute inset-0 opacity-0 group-hover:opacity-100 group-hover:blur transition duration-300 text-cyan-400 to-indigo-600 group-hover:animate-pulse">
+                    About
+                </span>
+            </a>
+        </li>
+        <li>
+            <a href="/projects" class="group relative transition duration-300">
+                <span class="group-hover:text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-indigo-600">
+                    Projects
+                </span>
+                <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-gradient-to-r from-cyan-400 to-indigo-600"></span>
+                <span class="absolute inset-0 opacity-0 group-hover:opacity-100 group-hover:blur transition duration-300 text-cyan-400 to-indigo-600 group-hover:animate-pulse">
+                    Projects
+                </span>
+            </a>
+        </li>
+        <li>
+            <a href="/stacks" class="group relative transition duration-300">
+                <span class="group-hover:text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-indigo-600">
+                    Stacks
+                </span>
+                <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-gradient-to-r from-cyan-400 to-indigo-600"></span>
+                <span class="absolute inset-0 opacity-0 group-hover:opacity-100 group-hover:blur transition duration-300 text-cyan-400 to-indigo-600 group-hover:animate-pulse">
+                    Stacks
+                </span>
+            </a>
+        </li>
+        <li>
+            <a href="/blog" class="group relative transition duration-300">
+                <span class="group-hover:text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-indigo-600">
+                    Blog
+                </span>
+                <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-gradient-to-r from-cyan-400 to-indigo-600"></span>
+                <span class="absolute inset-0 opacity-0 group-hover:opacity-100 group-hover:blur transition duration-300 text-cyan-400 to-indigo-600 group-hover:animate-pulse">
+                    Blog
+                </span>
+            </a>
+        </li>
+    </ul>
+</nav>
+
+<main>
+    {@render children()}
+</main>
